1754e5b58ea95e69e1eafd59c009db128a266d01
Zamezetor <149831319+Zamezetor@users.noreply.github.com>
2024-05-04 19:47:45 -0700

    Completion and upload of 05-04-2024_D2L_Classes_Pets.py

--- D2L_Projects/05-04-2024_D2L_Classes_Pets.py
+++ D2L_Projects/05-04-2024_D2L_Classes_Pets.py
@@ -3,6 +3,36 @@
 # D2L Classes Assignment
 # CC BY-NC-SA 4.0
 # Imports 
-# Variables 
+# Variables
+# Classes
+class pets:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
+        
+    def get(self):
+        print(f"{self.name} is a {self.species} that is {self.age} years old.")
+
+    def set(self):
+        self.name = input("What Is The Pet's Name \n ► ")
+        self.species = input("What Is The Pet's Species \n ► ")
+        age_input = input("What Is The Pet's Age \n ► ")
+        while not isinstance(age_input, int):
+            try:
+                age_input = int(age_input)
+            except:
+                age_input = input("Please Input an Integer\nWhat Is The Pet's Age \n ► ")            
+        self.age = age_input
+        
+
 # Functions
+def main():
+    pet = pets("", "", "")
+    pet.set()
+    pet.get()
+
+
 # Code
+main()
+
